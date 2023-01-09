5aeb7632a33a723d153d79083230e91e404175c1
Salvatore Spina <saske0109@gmail.com>
2023-01-09 13:27:40 +0100

    Commands like the subject ask

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -5,3 +5,7 @@ echo $1 # == S_IP
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--bind-address=$1 --node-ip=$1 --write-kubeconfig-mode=644" sh -s -
 cp /var/lib/rancher/k3s/server/token $2
 echo "k3s installed [server]"
+
+# Export PATH and alias to use commands like in the subject
+echo 'alias k="kubectl"' >> ~/.bashrc
+echo 'export PATH="/usr/sbin/:$PATH"' >> ~/.bashrc
