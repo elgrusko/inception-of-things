87a435cf527dd39a0e98e0ce52088b9571da40cf
Salvatore Spina <saske0109@gmail.com>
2023-01-09 14:42:45 +0100

    fix: part 1 perfectly finished w/aliases

--- p1/scripts/debian.sh
+++ p1/scripts/debian.sh
@@ -1,3 +1,7 @@
 sudo iptables -F
 sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
 sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+
+# Export PATH and alias to use commands like in the subject
+echo 'alias k="kubectl"' >> /etc/profile
+echo 'export PATH="/usr/sbin/:$PATH"' >> /etc/profile
--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -4,7 +4,3 @@ echo "install k3s [agent]"
 echo $2 # == SW_IP
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://$1:6443 --node-ip $3 --token-file $2" sh -s -
 echo "k3s installed [agent]"
-
-# Export PATH and alias to use commands like in the subject
-echo 'alias k="kubectl"' >> ~/.bashrc
-echo 'export PATH="/usr/sbin/:$PATH"' >> ~/.bashrc
