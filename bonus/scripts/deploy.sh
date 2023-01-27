57d426d7ad445c6fbb547517e38d53cf56f98698
Salvatore Spina <saske0109@gmail.com>
2023-01-27 15:01:18 +0100

    fix: relative path in bonus manifests deployment

--- bonus/scripts/deploy.sh
+++ bonus/scripts/deploy.sh
@@ -1,5 +1,3 @@
-path_conf=/home/sasso/Documents/GitHub/inception-of-things/bonus/confs
-
 echo "=== Cleaning namespaces ==="
 kubectl delete namespace gitlab
 
@@ -7,5 +5,5 @@ echo "=== Creating namespaces ==="
 kubectl create namespace gitlab
 
 echo "=== Applying applications ==="
-kubectl apply -n argocd -f $path_conf/gitlab.yaml
-kubectl apply -n argocd -f $path_conf/projects.yaml
+kubectl apply -n argocd -f ../confs/gitlab.yaml
+kubectl apply -n argocd -f ../confs/projects.yaml
